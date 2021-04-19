1057e83a7355cc6cdd461e1bc1c659e11fa5c952
Anna <dahlstrom.anna1@gmail.com>
2021-04-20 00:42:40 +0200

    added config and simple start screen that i hope works for everyone

---
+++ app/src/main/java/com/example/fisk/start_activity.java
@@ -0,0 +1,13 @@
+package com.example.fisk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class start_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity_layout);
+    }
+}
