3f95d981c5558607281507e36cceb1396c83bd28
trekrabbor <lienlouise.pham@gmail.com>
2021-04-21 11:46:01 +0200

    Added a rotating blobfish as a second activity when pressing the start button

--- app/src/main/java/com/example/fisk/start_activity.java
+++ app/src/main/java/com/example/fisk/start_activity.java
@@ -1,13 +1,35 @@
 package com.example.fisk;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class start_activity extends AppCompatActivity {
+    ImageView image;
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity_layout);
+
+        image = findViewById(R.id.imageView3);
+
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,          Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(5000);
+        rotate.setInterpolator(new LinearInterpolator());
+        rotate.setRepeatCount(Animation.INFINITE);
+        image.startAnimation(rotate);
     }
+
+
 }
